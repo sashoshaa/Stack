4cbf9bb227861385abd938e54fa41d00c5d861bb
Александра Соснина <alexandra.sosnina.a@gmail.com>
2024-11-20 14:29:58 +0300

    изменение main.cpp

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -8,11 +8,16 @@
 		s.Push(3);
 		s.Push(4);
 		s.Push(2);
-		//cout << s.Pop() << endl;
+		
 		TCalc c;
 		c.SetInfix("(4-8+(9-6)^5+7/8+3*6)");
 		c.ToPostfix();
 		cout << c.CalcPostfix() << endl;
-		
+		c.SetInfix("4/8");
+		c.ToPostfix();
+		cout << c.CalcPostfix() << endl;
+		c.SetInfix("-1/5");
+		c.ToPostfix();
+		cout << c.CalcPostfix() << endl;
 		return 0;
 }
